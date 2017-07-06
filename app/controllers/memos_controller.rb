e75f1751ee7d85aa66713d9b27371a5f7b224cb3
Hyucksang <kwonbebrave@gmail.com>
2017-07-06 09:26:15 +0000

    controller완성, header완성, _form 완성, memos view와 memos edit 완성

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,27 +1,52 @@
 class MemosController < ApplicationController
-  #create
+  # Create
   def new
     @memo = Memo.new
   end
-  
+
   def create
+    @memo = Memo.new
+    @memo.user_id = params[:memo][:user_id]
+    @memo.title   = params[:memo][:title]
+    @memo.content = params[:memo][:content]
+    if @memo.save
+      #redirect_to memo_path(@memo)
+      redirect_to @memo
+    else
+      redirect_to :back
+    end
   end
-  
-  #read
+# Read
   def show
+    @memo = Memo.find(params[:id])
   end
 
   def index
+    @memos = Memo.all
   end
 
-  #update
+  # Update
   def edit
+    @memo = Memo.find(params[:id])
   end
-  
+
   def update
+    @memo = Memo.find(params[:id])
+    @memo.user_id = params[:memo][:user_id]
+    @memo.title   = params[:memo][:title]
+    @memo.content = params[:memo][:content]
+    if @memo.save
+      #redirect_to memo_path(@memo)
+      redirect_to @memo
+    else
+      redirect_to :back
+    end
   end
-  
-  #delete
+
+  # Destroy
   def destroy
+    @memo = Memo.find(params[:id])
+    @memo.destroy!
+    redirect_to root_path
   end
 end
