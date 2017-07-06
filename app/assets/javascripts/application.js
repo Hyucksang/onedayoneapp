5476543a8f838ab9ad0c872728358d348abd81ff
Hyucksang <kwonbebrave@gmail.com>
2017-07-06 08:52:30 +0000

    memo tinymce

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+//= require tinymce-jquery
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,27 @@
+class MemosController < ApplicationController
+  #create
+  def new
+    @memo = Memo.new
+  end
+  
+  def create
+  end
+  
+  #read
+  def show
+  end
+
+  def index
+  end
+
+  #update
+  def edit
+  end
+  
+  def update
+  end
+  
+  #delete
+  def destroy
+  end
+end
